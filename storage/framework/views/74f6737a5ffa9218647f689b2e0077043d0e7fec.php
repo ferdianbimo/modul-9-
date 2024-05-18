02bd2f2feabfcfde4bf033b6429de71f2b58f9ec
ferdianbimo <161963608+ferdianbimo@users.noreply.github.com>
2024-05-18 22:34:03 +0700

    Add files via upload

---
+++ storage/framework/views/74f6737a5ffa9218647f689b2e0077043d0e7fec.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('layouts.default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\atm_m\Praktikum\Modul8 lanjutan\resources\views/home.blade.php ENDPATH**/ ?>
